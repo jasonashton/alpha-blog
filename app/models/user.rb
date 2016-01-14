df996da1aac0eae549916d53f214052ea56caa66
Jason Ashton <jlashton@wpi.edu>
2016-01-14 09:09:04 -0500

    updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-            uniqueness: { case_sensitive: false },
-            length: { maximum: 105},
-            format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 105 },
+  uniqueness: { case_sensitive: false },
+  format: { with: VALID_EMAIL_REGEX }
 end
